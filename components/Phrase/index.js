e732d2da2b01b57743548229e18c51a7e87cbb40
william-hayman <126586623+william-hayman@users.noreply.github.com>
2024-08-02 21:05:58 +0400

    starting

--- components/Phrase/index.js
+++ components/Phrase/index.js
@@ -5,14 +5,14 @@ const FlipWordsDemo = () => {
   const words = ["effective", "creative", "innovative", "strategic"];
 
   return (
-    <div className="h-[10rem] flex justify-center items-center px-4">
+    <div className="min-h-[10rem] flex justify-center items-center px-4 overflow-hidden">
       <div className="text-4xl mx-auto font-normal text-black">
         Drive
         <FlipWords
           words={words}
-          duration={3000}
-          className="text-orange-500"
-        />{" "}
+          duration={4000}
+          className="text-orange-500 flex md:inline -mb-10 -ml-2 md:ml-0"
+        />
         <br />
         marketing campaigns with us!
       </div>
--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,15 @@
 import Header from "@/components/Header/index";
 import Phrase from "./Phrase";
+import Image from "next/image";
+import logo from "@/public/logo-tag-orange.webp";
 
 export default function Navbar() {
   return (
     <div className="bg-white">
       <div className="w-full max-w-screen-xl mx-auto p-4 flex flex-wrap justify-between">
-        <div>Hay Branding</div>
+        <div>
+          <Image src={logo} alt="Logo" className="w-20" layout="fixed" />
+        </div>
         <div className="hidden md:flex">
           <Phrase />
         </div>
