af91413567676b7fca0afea249bfe2380dd13b14
william-hayman <126586623+william-hayman@users.noreply.github.com>
2024-08-12 18:59:41 +0400

    update

--- components/Phrase/index.js
+++ components/Phrase/index.js
@@ -5,13 +5,13 @@ const FlipWordsDemo = () => {
   const words = ["effective", "creative", "innovative", "strategic"];
 
   return (
-    <div className="min-h-[10rem] flex justify-center items-center px-4 overflow-hidden">
-      <div className="text-4xl mx-auto font-normal text-black">
+    <div className="min-h-[8rem] flex justify-center items-top px-0 md:px-4 overflow-hidden font-mulish">
+      <div className="text-4xl mx-auto text-black font-medium">
         Drive
         <FlipWords
           words={words}
           duration={4000}
-          className="text-orange-500 flex md:inline -mb-10 -ml-2 md:ml-0"
+          className="text-orange-500 inline md:inline -mb-10 -ml-0 md:ml-0 font-black"
         />
         <br />
         marketing campaigns with us!
